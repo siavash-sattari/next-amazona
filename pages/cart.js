32cf6f5b39703933d746c47ac28624850baee20f
siavash sattari <siavash.sattari.75@gmail.com>
2022-10-04 14:58:51 -0700

    create login form

--- pages/cart.js
+++ pages/cart.js
@@ -82,7 +82,7 @@ function CartScreen() {
                 </div>
               </li>
               <li>
-                <button onClick={() => router.push('/shipping')} className='primary-button w-full'>
+                <button onClick={() => router.push('login?redirect=/shipping')} className='primary-button w-full'>
                   Check Out
                 </button>
               </li>
