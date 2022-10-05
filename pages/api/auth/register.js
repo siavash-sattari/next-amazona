c787e8d434ae1abc535d12c84d5cfef37031d16f
siavash sattari <siavash.sattari.75@gmail.com>
2022-10-05 08:12:01 -0700

    implement register user

---
+++ pages/api/auth/register.js
@@ -0,0 +1,56 @@
+import nc from 'next-connect';
+import bcrypt from 'bcryptjs';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+import { signToken } from '../../../utils/auth';
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (req.method !== 'POST') {
+    return;
+  }
+
+  if (!name || !email || !email.includes('@') || !password || password.trim().length < 5) {
+    res.status(422).json({
+      message: 'Validation error'
+    });
+    return;
+  }
+
+  await db.connect();
+
+  const existingUser = await User.findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already!' });
+    await db.disconnect();
+    return;
+  }
+
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password),
+    isAdmin: false
+  });
+
+  const user = await newUser.save();
+  
+  await db.disconnect();
+
+  const token = signToken(user);
+
+  res.send({
+    token,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    message: 'Created user!'
+  });
+});
+
+export default handler;
---
+++ pages/register.js
@@ -0,0 +1,123 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+
+export default function LoginScreen() {
+  const router = useRouter();
+  const { redirect } = router.query;
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/');
+    }
+  }, []);
+
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors }
+  } = useForm();
+
+  const submitHandler = async ({ name, email, password }) => {
+    try {
+      const { data } = await axios.post('/api/auth/register', {
+        name,
+        email,
+        password
+      });
+
+      dispatch({ type: 'USER_LOGIN', payload: data });
+      Cookies.set('userInfo', JSON.stringify(data));
+      router.push(redirect || '/');
+    } catch (err) {
+      toast.error(err.response.data ? err.response.data.message : err.message);
+    }
+  };
+
+  return (
+    <Layout title='Create Account'>
+      <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
+        <h1 className='mb-4 text-xl'>Create Account</h1>
+        <div className='mb-4'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            className='w-full'
+            id='name'
+            autoFocus
+            {...register('name', {
+              required: 'Please enter name'
+            })}
+          />
+          {errors.name && <div className='text-red-500'>{errors.name.message}</div>}
+        </div>
+
+        <div className='mb-4'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            {...register('email', {
+              required: 'Please enter email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email'
+              }
+            })}
+            className='w-full'
+            id='email'></input>
+          {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            {...register('password', {
+              required: 'Please enter password',
+              minLength: { value: 6, message: 'password is more than 5 chars' }
+            })}
+            className='w-full'
+            id='password'
+            autoFocus></input>
+          {errors.password && <div className='text-red-500 '>{errors.password.message}</div>}
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input
+            className='w-full'
+            type='password'
+            id='confirmPassword'
+            {...register('confirmPassword', {
+              required: 'Please enter confirm password',
+              validate: value => value === getValues('password'),
+              minLength: {
+                value: 6,
+                message: 'confirm password is more than 5 chars'
+              }
+            })}
+          />
+          {errors.confirmPassword && <div className='text-red-500 '>{errors.confirmPassword.message}</div>}
+          {errors.confirmPassword && errors.confirmPassword.type === 'validate' && <div className='text-red-500 '>Password do not match</div>}
+        </div>
+
+        <div className='mb-4 '>
+          <button className='primary-button'>Register</button>
+        </div>
+        <div className='mb-4 '>
+          Already have an account? &nbsp;
+          <Link href={`/login?redirect=${redirect || '/'}`}>Login</Link>
+        </div>
+      </form>
+    </Layout>
+  );
+}
