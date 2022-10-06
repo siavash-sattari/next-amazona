de0fc33777ba56c8d7ba0ffecc6d6a90f2e900a2
siavash sattari <siavash.sattari.75@gmail.com>
2022-10-06 06:28:25 -0700

    update user profile

---
+++ pages/api/auth/update-profile.js
@@ -0,0 +1,44 @@
+import nc from 'next-connect';
+import bcrypt from 'bcryptjs';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+import { signToken, isAuth } from '../../../utils/auth';
+
+const handler = nc();
+
+handler.use(isAuth);
+
+handler.put(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !email.includes('@') || (password && password.trim().length < 5)) {
+    res.status(422).json({
+      message: 'Validation error'
+    });
+    return;
+  }
+
+  await db.connect();
+
+  const user = await User.findById(req.user._id);
+  user.name = name;
+  user.email = email;
+  user.password = password ? bcrypt.hashSync(password) : user.password;
+
+  await user.save();
+
+  await db.disconnect();
+
+  const token = signToken(user);
+
+  res.send({
+    token,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    message: 'User updated successfully'
+  });
+});
+
+export default handler;
---
+++ pages/profile.js
@@ -0,0 +1,124 @@
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { Store } from '../utils/Store';
+import { getError } from '../utils/error';
+import Layout from '../components/Layout';
+
+export default function ProfileScreen() {
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    setValue,
+    formState: { errors }
+  } = useForm();
+
+  const { state, dispatch } = useContext(Store);
+
+  const router = useRouter();
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (!userInfo) {
+      return router.push('/login');
+    }
+    setValue('name', userInfo.name);
+    setValue('email', userInfo.email);
+  }, [userInfo]);
+
+  const submitHandler = async ({ name, email, password }) => {
+    try {
+      const { data } = await axios.put(
+        '/api/auth/update-profile',
+        {
+          name,
+          email,
+          password
+        },
+        { headers: { authorization: `Bearer ${userInfo.token}` } }
+      );
+      Cookies.set('userInfo', data);
+      toast.success('Profile updated successfully');
+      dispatch({ type: 'USER_LOGIN', payload: data });
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <Layout title='Profile'>
+      <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
+        <h1 className='mb-4 text-xl'>Update Profile</h1>
+
+        <div className='mb-4'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            className='w-full'
+            id='name'
+            autoFocus
+            {...register('name', {
+              required: 'Please enter name'
+            })}
+          />
+          {errors.name && <div className='text-red-500'>{errors.name.message}</div>}
+        </div>
+
+        <div className='mb-4'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            className='w-full'
+            id='email'
+            {...register('email', {
+              required: 'Please enter email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email'
+              }
+            })}
+          />
+          {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+        </div>
+
+        <div className='mb-4'>
+          <label htmlFor='password'>Password</label>
+          <input
+            className='w-full'
+            type='password'
+            id='password'
+            {...register('password', {
+              minLength: { value: 6, message: 'password is more than 5 chars' }
+            })}
+          />
+          {errors.password && <div className='text-red-500 '>{errors.password.message}</div>}
+        </div>
+
+        <div className='mb-4'>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input
+            className='w-full'
+            type='password'
+            id='confirmPassword'
+            {...register('confirmPassword', {
+              validate: value => value === getValues('password'),
+              minLength: {
+                value: 6,
+                message: 'confirm password is more than 5 chars'
+              }
+            })}
+          />
+          {errors.confirmPassword && <div className='text-red-500 '>{errors.confirmPassword.message}</div>}
+          {errors.confirmPassword && errors.confirmPassword.type === 'validate' && <div className='text-red-500 '>Password do not match</div>}
+        </div>
+        <div className='mb-4'>
+          <button className='primary-button'>Update Profile</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
